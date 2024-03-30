b32deec9ab1fe714e7fb84616a1bb218b8e97d97
a3magic3pocket <a3magic3pocket@gmail.com>
2024-03-30 15:10:17 +0900

    Set the path to publish the static assets.

--- copystagram/src/main/java/com/copystagram/api/global/config/WebConfig.java
+++ copystagram/src/main/java/com/copystagram/api/global/config/WebConfig.java
@@ -1,11 +1,18 @@
 package com.copystagram.api.global.config;
 
+import java.time.Duration;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		// @formatter:off
@@ -19,4 +26,9 @@ public class WebConfig implements WebMvcConfigurer {
         // @formatter:on
 	}
 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/public/**").addResourceLocations("/public", "classpath:/static/")
+				.setCacheControl(CacheControl.maxAge(Duration.ofDays(365)));
+	}
 }
